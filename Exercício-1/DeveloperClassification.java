c7cb52384185bded47f0f28f0c5884d873e6ba18
rphsantoss <raapphaaeell@gmail.com>
2025-11-17 11:04:28 -0300

    feat: exercicio 1

---
+++ Exercício-1/DeveloperClassification.java
@@ -0,0 +1,13 @@
+public class DeveloperClassification {
+
+    public String classify(int years, int projects) {
+        if (years < 0 || projects < 0) {
+            throw new IllegalArgumentException("Os valores de anos de experiência e quantidade de projetos não podem ser negativos.");
+        }
+        if (years < 1 && projects < 2) {
+            return "Estagiário";
+        } else {
+            return "Júnior";
+        }
+    }
+}
---
+++ Exercício-1/Main.java
@@ -0,0 +1,28 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DeveloperClassification developerClassification = new DeveloperClassification();
+
+        try {
+            System.out.print("Quantos anos de experiência você tem em programação? ");
+            int years = scanner.nextInt();
+
+            System.out.print("Quantos projetos você já participou? ");
+            int projects = scanner.nextInt();
+
+            String classification = developerClassification.classify(years, projects);
+            System.out.println("Classificação: " + classification);
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Erro de formato! Digite apenas números inteiros positivos!.");
+        } 
+        catch (IllegalArgumentException e) {
+            System.out.println("Entrada inválida! " + e.getMessage());
+        }finally {
+            scanner.close();
+        }
+    }
+}
